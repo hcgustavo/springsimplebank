686227183155c39dff919dcb8a92aaaea1f2a6b0
Gustavo <ghcaixeta@gmail.com>
2025-04-26 21:00:45 -0400

    Initial configuration

---
+++ src/main/java/com/gustavohc/springsimplebank/SpringSimpleBankApplication.java
@@ -0,0 +1,13 @@
+package com.gustavohc.springsimplebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSimpleBankApplication.class, args);
+	}
+
+}
